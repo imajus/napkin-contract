d443bea0fed4e30229c6f9465107285589ee54a5
Denis Perov <denis.perov@gmail.com>
2024-08-05 09:56:36 +0500

    Update Meteor to 3.0.1

--- client/main.js
+++ client/main.js
@@ -2,10 +2,10 @@ import 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 import popper from 'popper.js';
-import { BootstrapHelpers } from 'meteor/imajus:bootstrap-helpers';
+// import { BootstrapHelpers } from 'meteor/imajus:bootstrap-helpers';
 import '/init';
 import './api';
 import './routes';
 
 global.Popper = popper; // fixes some issues with Popper and Meteor
-BootstrapHelpers.forBootstrap4 = true;
+// BootstrapHelpers.forBootstrap4 = true;
--- client/ui/home.js
+++ client/ui/home.js
@@ -1,1 +1,11 @@
+import { TemplateController } from 'meteor/imajus:template-controller';
 import './home.html';
+
+TemplateController('Home', {
+  state: {
+    title: null,
+  },
+  onCreated() {
+    this.state.title = 'Napkin Contract';
+  },
+});
